6b575199b17cf742d2d5e0a65d58c918e497e9fd
Mamatha Yarramsetti <ys.mamata@gmail.com>
2024-02-14 22:35:54 +0530

    npm parcel bundler

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 
-console.log('div out',document.querySelector('[id="root"]'));
-setTimeout(()=>{
+import React from "react";
+import ReactDOM from "react-dom";
+
+// console.log('div out',document.querySelector('[id="root"]'));
+// setTimeout(()=>{
     
-    const heading= React.createElement('h1',{},'Hellow World from React !');
+   
+// console.log('div',document.querySelector('[id="root"]'));
+// console.log('root 1',root);
+// },10000)
+
+const heading= React.createElement('h1',{id:"heading"},'Hellow World from React !');
 const root= ReactDOM.createRoot(document.getElementById('root'));
 console.log('root',root);
 root.render(heading);
-console.log('div',document.querySelector('[id="root"]'));
-console.log('root 1',root);
-},10000)
